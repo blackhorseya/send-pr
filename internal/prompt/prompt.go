355de21f05043c5e1286856e73ad6bbb6d98ebc5
blackhorseya <blackhorseya@gmail.com>
2025-02-21 14:33:22 +0800

    feat: embed and load templates with enhanced error handling
    - Embed a filesystem to load templates from the "templates" directory
    - Add error handling for loading templates in the `init` function
    - Introduce a `GetPromptString` function to retrieve prompt strings based on name and data
    - Create a `loadTemplates` function to read and parse template files from the embedded filesystem
    
    Signed-off-by: blackhorseya <blackhorseya@gmail.com>

--- internal/prompt/prompt.go
+++ internal/prompt/prompt.go
@@ -1,15 +1,70 @@
 package prompt
 
 import (
+	"bytes"
 	"embed"
+	"html/template"
+	"io/fs"
 )
 
-//go:embed templates/*
-var f embed.FS
+var (
+	//go:embed templates/*
+	f embed.FS
+
+	templates    map[string]*template.Template
+	templatesDir = "templates"
+)
 
 const (
 	SummarizePRDiffTemplate = "summarize_pr_diff.tmpl"
 )
 
 func init() {
+	err := loadTemplates(f)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// GetPromptString returns the prompt string for the given prompt name and data.
+func GetPromptString(name string, data map[string]interface{}) (string, error) {
+	tmpl, ok := templates[name]
+	if !ok {
+		return "", fs.ErrNotExist
+	}
+
+	var tpl bytes.Buffer
+	err := tmpl.Execute(&tpl, data)
+	if err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
+
+// loadTemplates loads all the templates found in the templates directory from the embedded filesystem.
+// It returns an error if reading the directory or parsing any template fails.
+func loadTemplates(files embed.FS) error {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	tmplFiles, err := fs.ReadDir(files, templatesDir)
+	if err != nil {
+		return err
+	}
+
+	for _, tmpl := range tmplFiles {
+		if tmpl.IsDir() {
+			continue
+		}
+
+		pt, err := template.ParseFS(files, templatesDir+"/"+tmpl.Name())
+		if err != nil {
+			return err
+		}
+
+		templates[tmpl.Name()] = pt
+	}
+
+	return nil
 }
